a8c217ff6faee5317bc55854512be8806daddfd1
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-07-28 22:24:57 -0400

    Fix wcapi .gitignore, add middleware
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ cmd/wcapi/main.go
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) Alexander Scheel
+ *
+ * Licensed under the terms of the AGPLv3.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+
+	"git.cipherboy.com/WordCorp/api/internal/database"
+	"git.cipherboy.com/WordCorp/api/pkg/auth"
+	"git.cipherboy.com/WordCorp/api/pkg/user"
+)
+
+const db_fmt string = "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s"
+
+var db_host string
+var db_port int
+var db_user string
+var db_password string
+var db_name string
+var db_sslmode string
+
+func gorillaWalkFn(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+	path, _ := route.GetPathTemplate()
+	log.Println("Routing path:", path)
+	return nil
+}
+
+func main() {
+	var err error
+
+	var addr string
+	var dbconn string
+	var debug bool
+
+	flag.StringVar(&addr, "addr", "localhost:8042", "Address to listen for HTTP requests on")
+	flag.StringVar(&db_host, "db_host", "localhost", "Hostname to contact the database over")
+	flag.IntVar(&db_port, "db_port", 5432, "Port to contact the database on")
+	flag.StringVar(&db_user, "db_user", "psql", "Username to contact the database with")
+	flag.StringVar(&db_password, "db_password", "password", "Password to authentication against the database with")
+	flag.StringVar(&db_name, "db_name", "wordcorpdb", "Database to connect to with")
+	flag.StringVar(&db_sslmode, "db_sslmode", "require", "SSL Validation mode (require, verify-full, verify-ca, or disable)")
+	flag.BoolVar(&debug, "debug", false, "Enable extra debug information")
+	flag.Parse()
+
+	// Open Database connection first
+	dbconn = fmt.Sprintf(db_fmt, db_host, db_port, db_user, db_password, db_name, db_sslmode)
+
+	if debug {
+		log.Println("Database connection string", dbconn)
+	}
+
+	err = database.OpenDatabase("postgres", dbconn)
+	if err != nil {
+		panic(err)
+	}
+	defer database.DB.Close()
+
+	// Add our user and authentication routers
+	router := mux.NewRouter()
+	auth.BuildRouter(router)
+	user.BuildRouter(router)
+
+	// Add proxy-headers middleware
+	handler := handlers.ProxyHeaders(router)
+
+	// Add logging middleware
+	handler = handlers.CombinedLoggingHandler(os.Stderr, handler)
+
+	// Add recovery middleware
+	handler = handlers.RecoveryHandler()(handler)
+
+	// Build our server and start it
+	srv := &http.Server{
+		Handler: handler,
+		Addr:    addr,
+	}
+
+	if debug {
+		err = router.Walk(gorillaWalkFn)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Println("Listening on " + addr)
+	log.Fatal(srv.ListenAndServe())
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module git.cipherboy.com/WordCorp/api
 go 1.14
 
 require (
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
-	github.com/lib/pq v1.7.1
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899
+	github.com/lib/pq v1.8.0
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
 )
