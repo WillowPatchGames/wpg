261ded20910b170d93cf75a5b3ad72cd3497eeb6
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-09-17 11:50:27 -0400

    Add game delete API endpoint
    Currently sets the state to finished to prevent it from being played.
    Later we should introduce a deleted lifecycle, but that requires
    database changes which aren't good right now.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ pkg/api/game/delete_handler.go
@@ -0,0 +1,101 @@
+package game
+
+import (
+	"log"
+	"net/http"
+
+	"git.cipherboy.com/WillowPatchGames/wpg/internal/database"
+	"git.cipherboy.com/WillowPatchGames/wpg/internal/models"
+	"git.cipherboy.com/WillowPatchGames/wpg/internal/utils"
+
+	api_errors "git.cipherboy.com/WillowPatchGames/wpg/pkg/errors"
+	"git.cipherboy.com/WillowPatchGames/wpg/pkg/middleware/auth"
+	"git.cipherboy.com/WillowPatchGames/wpg/pkg/middleware/hwaterr"
+)
+
+type deleteHandlerData struct {
+	GameID   uint64 `json:"id,omitempty" query:"id,omitempty" route:"GameID,omitempty"`
+	APIToken string `json:"api_token,omitempty" header:"X-Auth-Token,omitempty" query:"api_token,omitempty"`
+}
+
+type deleteHandlerResponse struct {
+	GameID uint64 `json:"id"`
+	Status string `json:"status"`
+}
+
+type DeleteHandler struct {
+	auth.Authed
+	hwaterr.ErrableHandler
+	utils.HTTPRequestHandler
+
+	req  deleteHandlerData
+	resp deleteHandlerResponse
+	user *models.UserModel
+}
+
+func (handle DeleteHandler) GetResponse() interface{} {
+	return handle.resp
+}
+
+func (handle *DeleteHandler) GetObjectPointer() interface{} {
+	return &handle.req
+}
+
+func (handle *DeleteHandler) GetToken() string {
+	return handle.req.APIToken
+}
+
+func (handle *DeleteHandler) SetUser(user *models.UserModel) {
+	handle.user = user
+}
+
+func (handle *DeleteHandler) ServeErrableHTTP(w http.ResponseWriter, r *http.Request) error {
+	if handle.req.GameID == 0 {
+		return hwaterr.WrapError(api_errors.ErrMissingRequest, http.StatusBadRequest)
+	}
+
+	tx, err := database.GetTransaction()
+	if err != nil {
+		log.Println("Transaction?", err)
+		return err
+	}
+
+	var game models.GameModel
+	err = game.FromID(tx, handle.req.GameID)
+	if err != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			log.Print("Unable to rollback:", rollbackErr)
+		}
+
+		log.Println("Getting game?", err)
+		return err
+	}
+
+	if game.OwnerID != handle.user.ID {
+		return hwaterr.WrapError(api_errors.ErrAccessDenied, http.StatusUnauthorized)
+	}
+
+	game.Lifecycle = "finished"
+
+	err = game.Save(tx)
+	if err != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			log.Print("Unable to rollback:", rollbackErr)
+		}
+
+		log.Println("Saving game?", err)
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Println("Commiting?", err)
+		return err
+	}
+
+	handle.resp.GameID = game.ID
+	handle.resp.Status = "deleted"
+
+	utils.SendResponse(w, r, handle)
+	return nil
+}
--- pkg/api/game/router.go
+++ pkg/api/game/router.go
@@ -24,8 +24,14 @@ func BuildRouter(router *mux.Router, debug bool) {
 		return auth.Require(inner)
 	}
 
+	var deleteFactory = func() parsel.Parseltongue {
+		inner := new(DeleteHandler)
+		return auth.Require(inner)
+	}
+
 	router.Handle("/api/v1/game/find", parsel.Wrap(queryFactory, config)).Methods("GET")
 	router.Handle("/api/v1/game/{GameID:[0-9]+}", parsel.Wrap(queryFactory, config)).Methods("GET")
+	router.Handle("/api/v1/game/{GameID:[0-9]+}", parsel.Wrap(deleteFactory, config)).Methods("DELETE")
 	router.Handle("/api/v1/game", parsel.Wrap(queryFactory, config)).Methods("GET")
 	router.Handle("/api/v1/games", parsel.Wrap(createFactory, config)).Methods("POST")
 
