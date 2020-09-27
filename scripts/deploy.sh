3f469bb375bb5c1d2400467779e8558e07b91980
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-09-27 00:50:22 -0400

    Revert "Add beta.willowpatchgames.com auto-deployment"
    This reverts commit ef6e6f92b600b2711a39231a0ba57e3a56b589a9.

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat ~/.ssh/id_ed25519 <<< "$BETA_DEPLOY_SSH_KEY"
-echo "beta.willowpatchgames.com,198.199.79.217 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBD11Fl5T69bFHQqZjEekGRksG8uuPoI4JzDNcUuu0BGwGG7wZsuQ4WRuVYzJ1hDJCEn7HJtqm0m/uSGRAn1qBxM=" >> "$HOME/.ssh/known_hosts"
-make beta-deploy
