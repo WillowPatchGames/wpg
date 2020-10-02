5bd5a7b39ce648de35bd71052b7742129d19af72
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-10-01 20:09:08 -0400

    Make sure wpgapi isn't running prior to testing
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- scripts/api-tests.sh
+++ scripts/api-tests.sh
@@ -3,7 +3,8 @@
 PYTHON="${1:-python3}"
 
 (
-  rm -f wpg.sqlite3 && ./wpgapi -db_type=sqlite -db_host=wpg.sqlite3 -db_sslmode=disable -db_name=wpgdb -debug=true -static_path="http://127.0.0.1:3000" -addr="0.0.0.0:8042"
+  killall wpgapi ; rm -f wpg.sqlite3
+	./wpgapi -db_type=sqlite -db_host=wpg.sqlite3 -db_sslmode=disable -db_name=wpgdb -debug=true -static_path="http://127.0.0.1:3000" -addr="0.0.0.0:8042" || true
 ) &
 
 (
@@ -11,5 +12,8 @@ PYTHON="${1:-python3}"
   $PYTHON -c 'import pytest' || $PYTHON -m pip install pytest
   cd test/api && $PYTHON -m pytest
 )
+ret=$?
 
 kill %1 || true
+
+exit $ret
