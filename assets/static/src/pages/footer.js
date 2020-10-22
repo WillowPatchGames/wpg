d9743c1752c19064452206984d28f1473e1b6254
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-10-21 23:24:03 -0400

    Add link to our blog in the footer
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- assets/static/src/pages/footer.js
+++ assets/static/src/pages/footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
       return (
         <div className="App-footer">
           <p style={{ fontSize: '0.85em' }} >
-            <Link to="/about">About Us</Link> | <Link to="/privacy">Privacy Policy</Link> | <Link to="/rules/rush">Rush! Rules</Link> | <Link to="/docs">Documentation</Link> | <Link to="/pricing">Pricing</Link>
+            <Link to="/about">About Us</Link> | <a href="https://blog.willowpatchgames.com">Blog</a> | <Link to="/privacy">Privacy Policy</Link> | <Link to="/rules/rush">Rush! Rules</Link> | <Link to="/docs">Documentation</Link> | <Link to="/pricing">Pricing</Link>
           </p>
           <p style={{ fontSize: '0.85em' }} >
             Questions or comments? <a href="mailto:willowpatchgames@gmail.com">Email us</a> or <a href="https://twitter.com/willowpatchgame">tweet us</a>.
