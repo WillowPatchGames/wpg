7c7fd1acf35323a14bfc86aacc19c1afd9f52fd6
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-10-23 21:24:16 -0400

    Introduce new plans ; add gotime helper
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ scripts/gotime.py
@@ -0,0 +1,14 @@
+nanosecond  = 1
+microsecond = 1000 * nanosecond
+millisecond = 1000 * microsecond
+second      = 1000 * millisecond
+minute      = 60 * second
+hour        = 60 * minute
+day         = 24 * hour
+week        = 7 * day
+month       = 30 * day
+year        = 365 * day
+decade      = 10 * year
+century     = 100 * year
+
+assert century == 3153600000000000000
