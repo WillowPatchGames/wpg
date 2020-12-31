d3aaf8cc91067b01998fdfe3d7d006e7aeddf598
Nicholas Scheel <monoidmusician@gmail.com>
2020-12-30 20:58:19 -0500

    Add more to the test page

--- assets/static/src/pages/test.js
+++ assets/static/src/pages/test.js
@@ -41,34 +41,174 @@ import { WebSocketController } from '../games/common.js';
 import { UserCache, GameCache } from '../utils/cache.js';
 import { gravatarify } from '../utils/gravatar.js';
 
+var StringSchema = {
+};
+var NumberSchema = {
+  "type": "number",
+};
+
+var schemas = {
+  "rush": {
+    "message": {
+      "message": StringSchema,
+    },
+    "play": {
+      "tile_id": NumberSchema,
+      "x": NumberSchema,
+      "y": NumberSchema,
+    },
+  },
+};
+
+function MakeSchemas(mode) {
+  var schema = schemas[mode];
+  if (!schema) return { renders: {}, refs: {}, gets: {} };
+  var refs = {};
+  var renders = {};
+  var gets = {};
+  var getter = type => () => {
+    var r = {};
+    for (let prop in schema[type]) {
+      r[prop] = refs[type][prop].current.value;
+      if (r[prop] === "") delete r[prop];
+      console.log(refs[type][prop].current);
+      if (refs[type][prop].current.type === 'number' && r[prop] !== undefined) {
+        r[prop] = +r[prop];
+      }
+    }
+    return r;
+  };
+  for (let type in schema) {
+    refs[type] = {};
+    renders[type] = [];
+    gets[type] = getter(type);
+    for (let prop in schema[type]) {
+      var ref = refs[type][prop] = React.createRef();
+      renders[type].push(((type,prop,ref) =>
+        <TextField key={ prop } label={ prop }
+          inputRef={ ref }
+          {... schema[type][prop] }
+        />
+      )(type,prop,ref));
+    }
+  }
+  return {
+    renders,
+    refs,
+    gets,
+  };
+}
+
 class TestGamePage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {status:"",messages:[],mode:'rush'};
-    this.msgRef = React.createRef();
+    this.state = {status:"",messages:[],mode:'rush',compose:"",state:{}};
+    this.schema = MakeSchemas();
   }
   render() {
     var open = this.state.wsController;
+    function message(v) {
+      var textAlign = v.sent ? "right" : "left";
+      var message_info = "";
+      var color = undefined;
+      switch (v.data.message_type) {
+        case "error":
+          color = "red"; message_info = v.data.error;
+          break;
+        case "admitted":
+          message_info = v.data.admitted;
+          break;
+        case "state":
+          color = "darkmagenta";
+        default:
+          if (v.data.error) {
+            color = "red"; message_info = v.data.error;
+          }
+          break;
+      }
+      if (message_info !== "") message_info = ": " + message_info;
+      return (
+        <>
+          <details style={{ textAlign }}>
+            <summary style={{ textAlign, outline: "none" }}>
+              <h4 style={{ textAlign, color, margin: "0", display: "inline" }}>{ v.data.message_type }{ message_info }</h4>
+            </summary>
+            <pre style={{
+                textAlign: "left",
+                marginLeft: v.sent ? "auto" : "0",
+                marginRight: v.sent ? "0" : "auto",
+                width: "fit-content",
+              }}>{JSON.stringify(v.data, null, 2)}</pre>
+          </details>
+        </>
+      )
+    }
+    if (!open) {
+      var n = Number(this.state.compose);
+      var compose_valid = n === n ? "" : "Please enter a numeric game id";
+    } else {
+      var is_json = this.state.compose !== "" && !/^\w+$/.test(this.state.compose);
+      if (is_json) {
+        compose_valid = "";
+        try {
+          JSON.parse(this.state.compose)
+        } catch(e) {
+          compose_valid += e;
+        }
+      } else {
+        compose_valid = "";
+      }
+    }
     return (<>
       <details key="details" style={{ maxHeight: "30vh", overflow: "auto" }}>
-        <summary>Details</summary>
+        <summary style={{ outline: "none" }}>Details</summary>
         <h2>Props</h2>
-        <pre style={{textAlign: "left", whiteSpace: "pre"}}>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre style={{ textAlign: "left", whiteSpace: "pre" }}>{JSON.stringify(this.props, null, 2)}</pre>
         <h2>State</h2>
-        <pre style={{textAlign: "left", whiteSpace: "pre"}}>{JSON.stringify(this.state, null, 2)}</pre>
+        <pre style={{ textAlign: "left", whiteSpace: "pre" }}>{JSON.stringify(this.state, null, 2)}</pre>
       </details>
-      <Select label="Game Mode" options={['rush','spades']} disabled={ open } value={ this.state.mode } onChange={ e => this.setState(state => {state.mode = e.target.value; return state}) } />
-      <ol key="messages">
-        {this.state.messages.map((v,k) => <li key={k}>{v.message || JSON.stringify(v.data)}</li>)}
-      </ol>
+      <Select label="Game Mode" options={['rush','spades']} disabled={ open }
+        value={ this.state.mode }
+        onChange={ e => {var v = e.target.value; this.setState(state => {state.compose = v; return state}) }} />
+      <div key="body" style={{ display: "flex", justifyContent: "space-between" }}>
+        <ol key="messages" style={{ margin: "0", flexGrow: 1, maxHeight: "70vh", overflow: "auto" }}>
+          {this.state.messages.map((v,k) => <li key={k}><div style={{
+            backgroundColor: "lightblue",
+            borderRadius: "10px",
+            padding: "6px",
+            margin: "12px 2px",
+            width: "fit-content",
+            marginLeft: v.sent ? "auto" : "2px",
+            marginRight: v.sent ? "2px" : "auto",
+          }}>{ message(v) }</div></li>)}
+        </ol>
+        <div key="state" style={{
+            width: "fit-content", maxWidth: "30%",
+            maxHeight: "70vh", overflow: "auto",
+            padding: "5px", flexGrow: 0,
+            display: "flex", flexDirection: "column-reverse",
+            backgroundColor: "rebeccapurple", color: "white",
+            borderBottomLeftRadius: "5px",
+          }}>
+          <pre style={{ textAlign: "left", whiteSpace: "pre" }}>{JSON.stringify(this.state.state, null, 2)}</pre>
+        </div>
+      </div>
       <form key="open-send" style={{ display: "flex", alignItems: "baseline" }} onSubmit={ e => {e.preventDefault(); open ? this.send(e) : this.open(e)} }>
-        <TextField fullwidth placeholder={ open ? "Message" : "Game ID" } inputRef={ this.msgRef } />
-        <Button label={ open ? "Send" : "Open" } type="submit" />
+        <div style={{ flexGrow: 1 }}>
+          <TextField fullwidth
+            value={ this.state.compose } invalid={ compose_valid !== "" }
+            label={ compose_valid || (open ? "Message" : "Game ID") }
+            onChange={ e => {var v = e.target.value; this.setState(state => {state.compose = v; return state}) }} />
+          <div class="fields" style={{ textAlign: "left" }}>
+            { this.schema.renders[this.state.compose] || [] }
+          </div>
+        </div>
+        <Button label={ open ? "Send" : "Open" } type="submit" unelevated ripple={false} disabled={ compose_valid !== "" } />
       </form>
     </>);
   }
   open() {
-    var id = this.msgRef.current.value;
+    var id = +this.state.compose;
     var mode = this.state.mode;
     console.log("OPEN", id, mode);
     var game = {
@@ -79,14 +219,16 @@ class TestGamePage extends React.Component {
     };
     var wsController = new WebSocketController(game);
     wsController.addEventListener("message", this.receive.bind(this));
+    this.schema = MakeSchemas(mode);
     this.setState(state => {
       state.game = game;
       state.wsController = wsController;
+      state.compose = "";
       return state;
     });
   }
   send() {
-    var message = this.msgRef.current.value;
+    var message = this.state.compose;
     try {
       message = JSON.parse(message);
       if (typeof message !== 'object') {
@@ -95,16 +237,20 @@ class TestGamePage extends React.Component {
     } catch(e) {
       message = { message_type: message };
     }
+    message = Object.assign((this.schema.gets[this.state.compose] || (() => {}))(), message);
     this.state.wsController.send(message);
     this.setState(state => {
-      state.messages.push(typeof message === 'string' ? { message } : { data: message });
+      state.messages.push({ sent: true, data: message });
       return state;
     });
   }
   receive(e) {
-    console.log(e);
     this.setState(state => {
-      state.messages.push({ data: JSON.parse(e.data), timestamp: e.timeStamp });
+      var data = JSON.parse(e.data);
+      state.messages.push({ sent: false, data: data, timestamp: e.timeStamp });
+      if (data.message_type === "state") {
+        state.state = data;
+      }
       return state;
     });
   }
