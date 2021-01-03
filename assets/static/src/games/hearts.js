b8ec800d47ac4b2aecf43d46c3e20d9a0055297b
Alexander Scheel <alexander.m.scheel@gmail.com>
2021-01-03 00:16:41 -0500

    Allow playing Hearts
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ assets/static/src/games/hearts.js
@@ -0,0 +1,183 @@
+import {
+  WebSocketController
+} from './common.js';
+
+import {
+  UserCache
+} from '../utils/cache.js';
+
+import {
+  CardHand,
+  Card,
+} from './card.js';
+
+class HeartsController {
+  constructor(game) {
+    this.game = game;
+
+    this.wsController = new WebSocketController(this.game);
+  }
+
+  async admitPlayer(player, admit, playing) {
+    return await this.wsController.send({
+      'message_type': 'admit',
+      'target_id': +player,
+      'admit': admit,
+      'playing': playing,
+    });
+  }
+
+  async markReady(ready) {
+    return await this.wsController.send({
+      'message_type': 'ready',
+      'ready': ready,
+    });
+  }
+
+  async startGame() {
+    return await this.wsController.sendAndWait({
+      'message_type': 'start',
+    });
+  }
+
+  async deal() {
+    return await this.wsController.sendAndWait({
+      'message_type': 'deal',
+    });
+  }
+
+  async pass(cards) {
+    return await this.wsController.sendAndWait({
+      'message_type': 'pass',
+      'to_pass': cards,
+    });
+  }
+
+  async play(card) {
+    return await this.wsController.send({
+      'message_type': 'play',
+      'card_id': +card,
+    });
+  }
+
+  onMessage(type, handler) {
+    return this.wsController.onMessage(type, handler);
+  }
+
+  close() {
+    this.wsController.close();
+  }
+}
+
+// Unlike Rush, where we have to duplicate logic on the client and server to
+// move and drop tiles &c, here we can lazily take values from the server and
+// blindly update ours. This is because we only do a single action at a time,
+// and unless there's a network glitch (in which case server wins anyways),
+// the data always aligns after the message is confirmed by the server.
+class HeartsData {
+  constructor(game) {
+    this.game = game;
+  }
+}
+
+class HeartsGame {
+  constructor(game, readonly) {
+    this.game = game;
+
+    if (readonly === undefined || readonly === null || readonly === false) {
+      this.controller = new HeartsController(game);
+      this.controller.onMessage("state", (data) => { this.handleNewState(data) });
+      this.controller.onMessage("synopsis", (data) => { this.handleNewSynopsis(data) });
+    }
+
+    this.data = new HeartsData(game);
+    this.synopsis = {};
+
+    this.started = false;
+    this.dealt = false;
+    this.passed = false;
+    this.finished = false;
+
+    this.onChange = () => {};
+  }
+
+  async handleNewState(message) {
+    // Hearts is a simpler game than Rush. We can always take the hand from the
+    // server as this is a turn-based game. We won't get out of sync like Rush.
+
+    // Update some metadata about game progress.
+    this.started = message.started;
+    this.dealt = message.dealt;
+    this.passed = message.passed;
+    this.finished = message.finished;
+
+    // Then update the main data object.
+    this.data.hand = message?.hand ? CardHand.deserialize(message.hand) : null;
+    if (this.data.hand != null) {
+      this.data.hand.cardSort(true, true);
+    }
+    this.data.have_passed = message?.have_passed;
+    this.data.incoming = message?.incoming ? CardHand.deserialize(message.incoming) : null;
+    if (this.data.incoming != null) {
+      this.data.incoming.cardSort(true, true);
+    }
+    this.data.tricks = message?.tricks;
+    this.data.round_score = message?.round_score;
+    this.data.score = message?.score;
+    this.data.turn = message?.turn;
+    this.data.leader = message?.leader;
+    this.data.dealer = message?.dealer;
+    this.data.pass_direction = message?.pass_direction;
+    this.data.played = message?.played ? CardHand.deserialize(message.played) : null;
+    this.data.who_played = [];
+    for (let uid of message.who_played) {
+      let player = await UserCache.FromId(uid);
+      this.data.who_played.push(player);
+    }
+    this.data.hearts_broken = message?.hearts_broken;
+    this.data.history = message?.history ? message.history.map(CardHand.deserialize) : null;
+    this.data.crib = message?.crib;
+    this.data.config = message?.config;
+
+    this.onChange(this);
+  }
+
+  async handleNewSynopsis(message) {
+    // Hearts is a simpler game than Rush. We can always take the hand from the
+    // server as this is a turn-based game. We won't get out of sync like Rush.
+    if (message.players) {
+      for (let player of message.players) {
+        player.user = await UserCache.FromId(player.user);
+      }
+    }
+    Object.assign(this.synopsis, message);
+
+    this.onChange(this);
+  }
+
+  my_turn() {
+    return +this.data.turn === +this.game.user.id;
+  }
+
+  async deal() {
+    return this.controller.deal();
+  }
+
+  async pass(cards) {
+    return this.controller.pass(cards);
+  }
+
+  async play(card) {
+    return this.controller.play(card);
+  }
+
+  close() {
+    this.controller.close();
+  }
+}
+
+export {
+  HeartsData,
+  HeartsGame,
+  HeartsController,
+};
---
+++ assets/static/src/pages/games/hearts.js
@@ -0,0 +1,429 @@
+import React from 'react';
+
+import '../../main.scss';
+
+import { gravatarify } from '../../utils/gravatar.js';
+
+import { Avatar } from '@rmwc/avatar';
+import '@rmwc/avatar/styles';
+import { Button } from '@rmwc/button';
+import '@rmwc/card/styles';
+import * as c from '@rmwc/card';
+import '@rmwc/button/styles';
+import { Select } from '@rmwc/select';
+import '@rmwc/select/styles';
+import { CircularProgress } from '@rmwc/circular-progress';
+import '@rmwc/circular-progress/styles';
+
+import { HeartsGame } from '../../games/hearts.js';
+import { CardSuit, CardImage } from '../../games/card.js';
+import { loadGame, addEv, notify } from '../games.js';
+import { UserCache } from '../../utils/cache.js';
+
+class HeartsGameComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.game = props.game;
+    this.state.selected = null;
+    this.state.pass_select = new Set();
+    this.state.pass = null;
+    this.state.pass_suggesting = true;
+    this.state.last_hand = null;
+    // FIXME: hack?
+    let old_handler = this.state.game.interface.onChange;
+    this.state.game.interface.onChange = () => {
+      old_handler();
+      this.setState(state => {
+        // Jinx
+        return state;
+      });
+    };
+  }
+  clearSelectAnd(then) {
+    return (...arg) => {
+      this.setState(state => Object.assign(state, {
+        selected: null,
+        pass_select: new Set(),
+        pass_suggesting: true,
+      }));
+      return then && then(...arg);
+    };
+  }
+  selecting(card) {
+    return Object.assign(card, {
+      selected: this.state.selected === card.id,
+      onClick: () => {
+        this.setState(state => {
+          if (state.selected === card.id)
+            state.selected = null;
+          else
+            state.selected = card.id;
+          return state;
+        });
+      },
+    });
+  }
+  selecting_pass(card) {
+    return Object.assign(card, {
+      selected: this.state.pass_select.has(card.id),
+      onClick: () => {
+        this.setState(state => {
+          if (state.pass_select.has(card.id))
+            state.pass_select.delete(card.id);
+          else
+            state.pass_select.add(card.id);
+          return state;
+        });
+      },
+    });
+  }
+  captureHandAnd(then) {
+    return (...arg) => {
+      this.setState(state => Object.assign(state, {
+        last_hand: this.state.game.interface.data.hand?.cards.map(card => card.id),
+      }));
+      return then && then(...arg);
+    };
+  }
+  showing_new(card) {
+    if (!this.state.last_hand) return;
+    return Object.assign(card, {
+      selected: !this.state.last_hand.includes(card.id),
+      onClick: () => {
+        this.setState(state => {
+          state.last_hand = null;
+          return state;
+        });
+      },
+    });
+  }
+  render() {
+    var status = a => <h3>{ a }</h3>;
+    var big_status = a => <h2>{ a }</h2>;
+    // Properties used for display card hands
+    var handProps = {
+      overlap: true,
+      curve: true,
+      scale: 0.50,
+    };
+    if (!this.state.game.interface.started) {
+      return status("Waiting for game to start …");
+    } else if (this.state.game.interface.finished) {
+      return <div>
+        {status("Finished")}
+      </div>;
+    } else if (!this.state.game.interface.passed) {
+      if (!this.state.game.interface.data.have_passed) {
+        return <div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+            <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+              <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                {status("Please select " + this.state.game.config.number_to_pass + " cards to pass:")}
+                <Button label="Pass" raised ripple={false} onClick={this.clearSelectAnd(() =>
+                  this.state.game.interface.pass(Array.from(this.state.pass_select)))
+                }/>
+              </div>
+            </c.Card>
+          </div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+              <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+                <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                  <h3>Hand</h3>
+                  { this.state.game.interface.data.hand?.toImage(this.selecting_pass.bind(this), handProps) }
+                </div>
+              </c.Card>
+            </div>
+        </div>;
+      } else {
+        return <div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+            <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+              <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                {status("Waiting for others to finish passing...")}
+                { this.state.game.interface.data.incoming ? status("Incoming Cards") : null }
+                { this.state.game.interface.data.incoming?.toImage(handProps) }
+              </div>
+            </c.Card>
+          </div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+              <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+                <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                  <h3>Hand</h3>
+                  { this.state.game.interface.data.hand?.toImage(this.selecting_pass.bind(this), handProps) }
+                </div>
+              </c.Card>
+            </div>
+        </div>;
+      }
+    } else {
+      var already_played = +this.state.game.interface.data.played.cards.length;
+      var num_players = +this.state.game.config.num_players;
+      if (this.state.game.interface.my_turn()) {
+        var leading = !already_played || already_played >= num_players;
+        return <div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+            <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+              <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                { this.state.game.interface.data.incoming ? status("Incoming Cards") : null }
+                { this.state.game.interface.data.incoming?.toImage(handProps) }
+                {status(leading ? (already_played ? "You took it, lead the next trick!" : "You lead off!") : already_played === 1 ? "This card was led" : "These cards have been played")}
+                { this.state.game.interface.data.played?.toImage() }
+                {big_status("Your turn to play")}
+                {status("Choose a card")}
+                <Button label={ this.state.selected ? "Play this card" : "Pick a card!" } unelevated ripple={false} disabled={ !this.state.selected }
+                  onClick={this.clearSelectAnd(() => this.state.game.interface.play(this.state.selected)) } />
+              </div>
+            </c.Card>
+          </div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+            <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+              <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                <h3>Hand</h3>
+                { this.state.game.interface.data.hand?.toImage(this.selecting.bind(this), handProps) }
+              </div>
+            </c.Card>
+          </div>
+        </div>;
+      } else {
+        return <div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+            <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+              <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                { this.state.game.interface.data.incoming ? status("Incoming Cards") : null }
+                { this.state.game.interface.data.incoming?.toImage(handProps) }
+                { status(this.state.game.interface.data.played.cards.length == num_players ? "Last Trick" : "Current Trick") }
+                { this.state.game.interface.data.played?.toImage() }
+                {status("Waiting for the other player" + (num_players < 3 ? "" : "s") + " to play …")}
+              </div>
+            </c.Card>
+          </div>
+          <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+            <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+              <div style={{ padding: "1rem 1rem 1rem 1rem" }}>
+                <h3>Hand</h3>
+                { this.state.game.interface.data.hand?.toImage(this.selecting.bind(this), handProps) }
+              </div>
+            </c.Card>
+          </div>
+        </div>;
+      }
+    }
+  }
+}
+
+class HeartsGameSynopsis extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = this.newState();
+
+    let old_handler = this.props.game.interface.onChange;
+    this.props.game.interface.onChange = () => {
+      old_handler();
+      this.setState(state => this.newState());
+    };
+  }
+
+  newState() {
+    let new_state = { indexed_players: {}, spectators: {}, suit: undefined };
+
+    if (this.props.game.interface.synopsis && this.props.game.interface.synopsis.players) {
+      for (let player of this.props.game.interface.synopsis.players) {
+        if (player.player_index !== -1) {
+          new_state.indexed_players[player.player_index] = player;
+        } else {
+          new_state.spectators[player.player_index] = player;
+        }
+      }
+    }
+    if (this.props.game.interface.data && this.props.game.interface.data.played) {
+      var i = this.props.game.interface;
+      if (i.passded && i.dealt && !i.finished) {
+        var num_played = this.props.game.interface.data.played.cards.length;
+        var num_players = this.props.game.interface.data.config.num_players;
+        if (num_played > 0 && num_played < num_players) {
+          new_state.suit = this.props.game.interface.data.played.cards[0].suit;
+        } else {
+          new_state.suit = "waiting";
+        }
+      } else if (!i.finished) {
+        new_state.suit = i.dealt ? "passding" : "dealing";
+      }
+    }
+
+    return new_state;
+  }
+
+  render() {
+    var sigil = (t,c) => <span style={{ fontSize: "170%", color: c }}>{ t }</span>
+    var synopsis_columns = {
+      "user":{
+        name: "User",
+        printer: (user,player) =>
+          <Avatar src={ gravatarify(user) } name={ user.display }
+            style={{ border: "2px solid "+(player.is_turn ? "#2acc0c" : "#FFFFFF00") }}
+            size={ user.id === this.props.user.id ? "xlarge" : "large" } />,
+      },
+      "is_leader":{
+        name: "Lead",
+        printer: (is_leader,player,state) =>
+          !is_leader || !state.suit
+          ? ""
+          : state.suit instanceof CardSuit
+          ? sigil(state.suit.toUnicode() || "♤", state.suit.toColor())
+          : state.suit === "waiting"
+          ? <CircularProgress size="xsmall" style={{ color: "#558abf" }} />
+          : sigil("♠"),
+      },
+      "is_dealer":{
+        name: "Dealer",
+        printer: a => a ? sigil("♠") : "",
+      },
+      "pass":{
+        name: "Bid",
+        printer: a => a === 0 ? "–" : a >= 19 ? HeartsGame.pass_names[a] : ""+a,
+      },
+      "tricks":"Tricks",
+      "score":"Score",
+      "overtakes":"Overtakes",
+    };
+
+    var tabulate = columns => data => {
+      if (!data) return [null];
+      var rows = [];
+      for (let dat of data) {
+        rows.push([]);
+        for (let k in columns) {
+          var printer = a => a;
+          if (typeof columns[k] === "object") printer = columns[k].printer;
+          rows[rows.length-1].push(<td key={ k }>{ printer(dat[k],dat,this.state) }</td>)
+        }
+      }
+      return rows.map((row, i) => <tr key={ data[i].user.id }>{row}</tr>);
+    };
+
+    var headings = [];
+    for (let k in synopsis_columns) {
+      var name = synopsis_columns[k];
+      if (typeof name === "object") name = name.name;
+      headings.push(<th key={ k }>{ name }</th>);
+    }
+
+    var player_rows = [];
+    if (this.state.indexed_players) {
+      var remaining = [];
+
+      for (let player_index of Object.keys(this.state.indexed_players).sort()) {
+        let player = this.state.indexed_players[player_index];
+        remaining.push(player);
+      }
+
+      player_rows.push(...tabulate(synopsis_columns)(remaining));
+    }
+
+    var player_view = null;
+    if (player_rows) {
+      player_view = <table style={{ "textAlign": "center" }}>
+        <tbody>
+          <tr key={ "hearts_synopsis_headings" }>
+            { headings }
+          </tr>
+          { player_rows }
+        </tbody>
+      </table>
+    }
+
+    return (
+      <div style={{ width: "90%" , margin: "0 auto 1em auto" }}>
+        <c.Card style={{ width: "100%" , padding: "0.5em 0.5em 0.5em 0.5em" }}>
+          <div className="text-left scrollable-x">
+            <b>Hearts</b>
+            { player_view }
+          </div>
+        </c.Card>
+      </div>
+    );
+  }
+}
+
+class HeartsGamePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countdown: null,
+    };
+
+    this.game = loadGame(this.props.game);
+    this.props.setGame(this.game);
+
+    let personalize = async (usr) => usr === this.props.user.id ? "You" : (await UserCache.FromId(usr)).display;
+    if (this.game) {
+      this.state.interface = this.game.interface;
+      this.unmount = addEv(this.game, {
+        "started": data => {
+          data.message = "Let the games begin!";
+          notify(this.props.snackbar, data.message, data.type);
+
+          if (!data.playing) {
+            this.props.setPage('afterparty');
+          }
+        },
+        "countdown": data => {
+          data.message = "Game starting in " + data.value;
+          this.setState(state => Object.assign({}, state, { countdown: data.value }));
+          setTimeout(() => this.setState(state => Object.assign({}, state, { countdown: null })), 1000);
+          this.state.interface.controller.wsController.send({'message_type': 'countback', 'value': data.value});
+
+        },
+        "draw": async (data) => {
+          data.message = await personalize(data.drawer) + " drew!";
+          notify(this.props.snackbar, data.message, data.type);
+        },
+        "finished": async (data) => {
+          data.message = await personalize(data.winner) + " won!";
+          notify(this.props.snackbar, data.message, data.type);
+          this.game.winner = data.winner;
+          this.props.setPage('afterparty');
+        },
+        "error": data => {
+          notify(this.props.snackbar, data.error, "error");
+        },
+        "": data => {
+          if (data.message) {
+            notify(this.props.snackbar, data.message, data.type);
+          }
+        },
+      });
+    }
+  }
+  componentDidMount() {
+    //this.props.setImmersive(true);
+  }
+  componentWillUnmount() {
+    if (this.unmount) this.unmount();
+    //this.props.setImmersive(false);
+  }
+  render() {
+    var countdown = null;
+    if (this.state.countdown !== null && this.state.countdown !== 0) {
+      countdown = <div className="countdown-overlay">
+        <div className="countdown-circle">
+          { this.state.countdown }
+        </div>
+      </div>
+    }
+
+    return (
+      <div>
+        { countdown }
+        <HeartsGameSynopsis game={ this.game } {...this.props} />
+        <HeartsGameComponent game={ this.game } interface={ this.state.interface } notify={ (...arg) => notify(this.props.snackbar, ...arg) } />
+      </div>
+    );
+  }
+}
+
+export {
+  HeartsGamePage,
+}
